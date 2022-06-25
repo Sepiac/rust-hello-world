14431be1ebff141c783c49d33c2e153c7e5cbfab
Dane Russell <danerussell4@gmail.com>
2022-06-25 14:05:46 -0400

    refactor(number_game): formatting

--- src/number_game.rs
+++ src/number_game.rs
@@ -1,48 +1,48 @@
 use rand::Rng;
-    use std::io;
-    use std::ops;
-    pub struct NumberGame {
-        pub secret_number: i32,
-        pub guessed_number: i32,
-    }
+use std::io;
+use std::ops;
+pub struct NumberGame {
+    pub secret_number: i32,
+    pub guessed_number: i32,
+}
 
-    impl NumberGame {
-        fn init(&mut self) {
-            println!("What do you think the secret number is?");
-            self.guessed_number = Self::get_guessed_number()
-        }
-        pub fn play(&mut self) {
-            self.init();
-            while self.secret_number != self.guessed_number {
-                if self.guessed_number > self.secret_number {
-                    println!("Incorrect! Try lower.")
-                } else if self.guessed_number < self.secret_number {
-                    println!("Incorrect! Try higher.")
-                }
-                self.guessed_number = Self::get_guessed_number();
+impl NumberGame {
+    fn init(&mut self) {
+        println!("What do you think the secret number is?");
+        self.guessed_number = Self::get_guessed_number()
+    }
+    pub fn play(&mut self) {
+        self.init();
+        while self.secret_number != self.guessed_number {
+            if self.guessed_number > self.secret_number {
+                println!("Incorrect! Try lower.")
+            } else if self.guessed_number < self.secret_number {
+                println!("Incorrect! Try higher.")
             }
-
-            println!("You have correctly guessed the number!");
-        }
-        fn generate_secret_number(range: ops::Range<i32>) -> i32 {
-            let mut rng = rand::thread_rng();
-            rng.gen_range(range)
+            self.guessed_number = Self::get_guessed_number();
         }
 
-        fn get_guessed_number() -> i32 {
-            let mut buffer = String::new();
-            io::stdin()
-                .read_line(&mut buffer)
-                .expect("Failed to read number from user");
-            buffer.trim().parse::<i32>().unwrap()
-        }
+        println!("You have correctly guessed the number!");
+    }
+    fn generate_secret_number(range: ops::Range<i32>) -> i32 {
+        let mut rng = rand::thread_rng();
+        rng.gen_range(range)
     }
 
-    impl Default for NumberGame {
-        fn default() -> Self {
-            Self {
-                secret_number: Self::generate_secret_number(1..10),
-                guessed_number: -1,
-            }
+    fn get_guessed_number() -> i32 {
+        let mut buffer = String::new();
+        io::stdin()
+            .read_line(&mut buffer)
+            .expect("Failed to read number from user");
+        buffer.trim().parse::<i32>().unwrap()
+    }
+}
+
+impl Default for NumberGame {
+    fn default() -> Self {
+        Self {
+            secret_number: Self::generate_secret_number(1..10),
+            guessed_number: -1,
         }
     }
+}
