28cbbdbb2592317fc1ca2a9c24a539fd72d17c89
Dane Russell <danerussell4@gmail.com>
2022-06-25 13:51:47 -0400

    refactor(main): move more functionality into NumberGame struct

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ struct NumberGame {
 impl NumberGame {
     fn init(&mut self) {
         println!("What do you think the secret number is?");
-        self.guessed_number = get_guessed_number()
+        self.guessed_number = Self::get_guessed_number()
     }
     fn play(&mut self) {
         self.init();
@@ -20,31 +20,30 @@ impl NumberGame {
             } else if self.guessed_number < self.secret_number {
                 println!("Incorrect! Try higher.")
             }
-            self.guessed_number = get_guessed_number();
+            self.guessed_number = Self::get_guessed_number();
         }
-    
+
         println!("You have correctly guessed the number!");
+    }
+    fn generate_secret_number(range: ops::Range<i32>) -> i32 {
+        let mut rng = rand::thread_rng();
+        rng.gen_range(range)
+    }
+
+    fn get_guessed_number() -> i32 {
+        let mut buffer = String::new();
+        io::stdin()
+            .read_line(&mut buffer)
+            .expect("Failed to read number from user");
+        buffer.trim().parse::<i32>().unwrap()
     }
 }
 
 fn main() {
-   let mut ng = NumberGame{
-    secret_number: generate_secret_number(1..101),
-    // TODO(sepiac): figure out how to leave this uninitialized
-    guessed_number: -1,
-   };
-   ng.play();
-}
-
-fn generate_secret_number(range: ops::Range<i32>) -> i32 {
-    let mut rng = rand::thread_rng();
-    rng.gen_range(range)
-}
-
-fn get_guessed_number() -> i32 {
-    let mut buffer = String::new();
-    io::stdin()
-        .read_line(&mut buffer)
-        .expect("Failed to read number from user");
-    buffer.trim().parse::<i32>().unwrap()
+    let mut ng = NumberGame {
+        secret_number: NumberGame::generate_secret_number(1..101),
+        // TODO(sepiac): figure out how to leave this uninitialized
+        guessed_number: -1,
+    };
+    ng.play();
 }
